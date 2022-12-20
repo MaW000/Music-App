5b8e89b1288af21924af6233f10cf66f8e4f6a0d
matt wardlow <mwardlow0@gmail.com>
2022-12-20 16:16:47 -0500

    style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#301008] opacity-100">
-        <img src='http://itunes.apple.com/favicon.ico' alt="logo" className="object-contain w-full h-14" />
+        <img src='https://icons8.com/icon/rI-3G2uuvZkr/music-heart' alt="logo" className="object-contain w-full h-14" />
         <NavLinks />
       </div>
 
